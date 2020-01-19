f8ddd9ed32b47f086d8bbefad40277e30348b6c0
rosered11 <krihsana5@gmail.com>
2020-01-19 19:02:12 +0700

    add service tweet

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('tweet/', views.tweet_list),
+    path('tweet/<int:pk>/', views.tweet_detail),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -1,8 +1,50 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from tweet.models import Tweet
+from tweet.serializers import TweetSerializer
 
-# Create your views here.
-from django.http import HttpResponse
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
 
+@api_view(['GET', 'POST'])
+def tweet_list(request, format=None):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        tweets = Tweet.objects.all()
+        serializer = TweetSerializer(tweets, many=True)
+        return Response(serializer.data)
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    elif request.method == 'POST':
+        serializer = TweetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def tweet_detail(request, pk, format=None):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

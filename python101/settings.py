aff348e7d5e8447491b8ee2f6606bc06acd7aa1d
rosered11 <krihsana5@gmail.com>
2020-01-19 20:13:23 +0700

    add rest framework and update url routing

--- python101/settings.py
+++ python101/settings.py
@@ -89,7 +89,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -127,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}

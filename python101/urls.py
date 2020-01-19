969a1be37b1172550c7e9b4ed4e4dce587773c6f
Panachai <panachai.ny@gmail.com>
2020-01-19 21:30:17 +0700

    add jwt

--- python101/urls.py
+++ python101/urls.py
@@ -18,7 +18,10 @@ from django.urls import path,include
 from django.conf.urls import url
 from django.contrib.auth.models import User
 from rest_framework import routers
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -26,4 +29,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('', include('tweet.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]

24a7172efe7f74ab600d3741f7eb6b61595bbc19
rosered11 <krihsana5@gmail.com>
2020-02-10 13:40:46 +0700

    fix unit test for author and owner

--- tweet/tests/test_models.py
+++ tweet/tests/test_models.py
@@ -1,19 +1,25 @@
-from tweet.models import Tweet
+from tweet.models import Tweet, User
 from django.test import TestCase
 from datetime import datetime
 
 
 class Test_models(TestCase):
     def setUp(self):
+        self.user1 = User.objects.create(
+            email='user1@user1.com'
+        )
+        self.user2 = User.objects.create(
+            email='user2@user2.com'
+        )
         Tweet.objects.create(
-            body="test", is_retweet=False, author="one_author", owner="one_owner")
+            body="test", is_retweet=False, author=self.user1, owner=self.user1)
         Tweet.objects.create(
-            body="test2", is_retweet=False, author="two_author", owner="two_owner")
+            body="test2", is_retweet=False, author=self.user2, owner=self.user2)
 
     def test_tweet(self):
         qs = Tweet.objects.all()
         self.assertEqual(qs.count(), 2)
-        tw1 = Tweet.objects.get(author="one_author")
-        tw2 = Tweet.objects.get(author="two_author")
+        tw1 = Tweet.objects.get(author=self.user1)
+        tw2 = Tweet.objects.get(author=self.user2)
         self.assertEqual(tw1.body, "test")
         self.assertEqual(tw2.body, "test2")

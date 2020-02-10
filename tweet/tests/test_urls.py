47b2452103ce4328cd8c3244729af6e5c7b8dd1a
Panachai <panachai.ny@gmail.com>
2020-02-10 20:04:58 +0700

    update integration test tweet

--- tweet/tests/test_urls.py
+++ tweet/tests/test_urls.py
@@ -1,19 +1,70 @@
 import pytest
+
 from django.urls import reverse, resolve
 from django.test import TestCase, Client, TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 
-class Test_urls(TestCase):
+class Test_tweets(TestCase):
 
     def setUp(self):
-        # Every test needs a client.
-        self.client = Client()
+        # arrange
+        self.client = APIClient()
+        self.user = User.objects.create_user('admin', 'example@example.com')
+        self.client.force_authenticate(user=self.user)
 
     def test_tweets_unauth(self):
-        response = self.client.get('/tweets')
-        self.assertEqual(response.status_code, 401)
+        # arrange
+        self.client.force_authenticate(user=None)
 
-    def test_tweets_unauth(self):
-        # self.client.force_login()
+        # action
         response = self.client.get('/tweets')
-        self.assertEqual(response.status_code, 401)
+
+        # assert
+        self.assertEqual(response.status_code,
+                         401,
+                         "The status code does not match.",)
+
+    # def test_tweets_auth(self):
+    #     # action
+    #     response = self.client.get('/tweets', follow=True)
+
+    #     # assert
+    #     self.assertEqual(response.status_code, 200,
+    #                      "The status code does not match.")
+
+
+class Test_health(TestCase):
+    def setUp(self):
+        # arrange
+        self.client = APIClient()
+        self.user = User.objects.create_user('admin', 'example@example.com')
+        self.client.force_authenticate(user=self.user)
+
+    def test_health(self):
+        # action
+        response = self.client.get('/api/health', follow=True)
+
+        # assert
+        self.assertEqual(response.status_code, 200,
+                         "The status code does not match.")
+
+    def test_health_with_auth(self):
+        # action
+        response = self.client.get('/api/health_auth', follow=True)
+
+        # assert
+        self.assertEqual(response.status_code, 200,
+                         "The status code does not match.")
+
+    def test_health_with_unauth(self):
+        # arrange
+        self.client.force_authenticate(user=None)
+
+        # action
+        response = self.client.get('/api/health_auth', follow=True)
+
+        # assert
+        self.assertEqual(response.status_code, 401,
+                         "The status code does not match.")

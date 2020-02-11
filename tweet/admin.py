14b82a23f498ac67a5ddc2b9f46e297d4fba126f
Panachai <panachai.ny@gmail.com>
2020-02-11 21:55:55 +0700

    add webadmin

--- tweet/admin.py
+++ tweet/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from tweet.models import Tweet, User
 
-# Register your models here.
+
+# admin.site.register(Tweet)
+# admin.site.register(User)
+
+
+class UserInstanceInline(admin.TabularInline):
+    model = User
+
+
+class TweetInstanceInline(admin.TabularInline):
+    model = Tweet
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'password', 'created_at', 'updated_at')
+
+    # inlines = [TweetInstanceInline]
+    ordering = ('id', 'email')
+    search_fields = ('email', 'created_at')
+
+    fields = ['email', 'password', ('created_at', 'updated_at')]
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_filter = ('created_at', 'updated_at', 'is_retweet')
+    # inlines = [UserInstanceInline]
+
+    fieldsets = (
+        ('Detail',  {
+            'fields': ('body', 'is_retweet')
+        }),
+        ('Author', {
+            'fields': ('author', 'owner')
+        }),
+        ('Date', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
